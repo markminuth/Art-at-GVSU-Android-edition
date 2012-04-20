f0e06983a9f70b90a91c14a1b729bc43003b9642
Jonathan Bardsley <bardslej@mail.gvsu.edu>
2012-04-20 09:25:15 -0400

    possibly working share feature, unable to test without device

---
+++ version1.0/src/edu/artAtGVSU/Campus.java
@@ -0,0 +1,42 @@
+package edu.artAtGVSU;
+
+import java.util.ArrayList;
+
+public class Campus {
+	String campusName;
+	String campusID;
+	ArrayList<Building> buildings;
+	
+	public Campus(String cName, String cID){
+		campusName = cName;
+		campusID = cID;
+		buildings = null;
+	}
+
+	public void setCampusName(String campusName) {
+		this.campusName = campusName;
+	}
+
+	public void setCampusID(String campusID) {
+		this.campusID = campusID;
+	}
+
+	public void setBuildings(ArrayList<Building> buildings) {
+		this.buildings = buildings;
+	}
+
+	public String getCampusName() {
+		return campusName;
+	}
+
+	public String getCampusID() {
+		return campusID;
+	}
+
+	public ArrayList<Building> getBuildings() {
+		return buildings;
+	}
+	
+	
+	
+}
