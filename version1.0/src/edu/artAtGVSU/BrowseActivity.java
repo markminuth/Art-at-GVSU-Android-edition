b67b580dd541e8cda197d8f5853f399fcd67734c
Aaron Merritt <merritta87@gmail.com>
2012-04-21 17:34:45 -0400

    Updated browse and added images for browsing

--- version1.0/src/edu/artAtGVSU/BrowseActivity.java
+++ version1.0/src/edu/artAtGVSU/BrowseActivity.java
@@ -1,13 +1,53 @@
 package edu.artAtGVSU;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
 
-public class BrowseActivity extends Activity{
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+
+public class BrowseActivity extends Activity {
+	ArrayList<Bitmap> buildingArt;
+	ListView list;
+	BrowseImageAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.browse);
+		list = (ListView) findViewById(R.id.browseList);
+
+		buildingArt = new ArrayList<Bitmap>();
+		for (int i = 2; i < 7; ++i) {
+			int resID = getResources().getIdentifier("loc_" + i, "drawable", "edu.artAtGVSU");
+			Bitmap building = BitmapFactory.decodeResource(getResources(), resID);
+			buildingArt.add(building);
+		}
+
+		adapter = new BrowseImageAdapter(this, R.layout.browselist_item, buildingArt);
+		list.setAdapter(adapter);
+		
+		list.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> arg0, View arg1, int pos, long arg3) 
+			{
+				Toast.makeText(getApplicationContext(), "This worked", Toast.LENGTH_SHORT).show();
+				// TODO Auto-generated method stub
+//				Toast.makeText(c, "WORKS", Toast.LENGTH_LONG);
+//				ArtWork seleted = searchedArtWork.get(pos);
+//				ArtWork a = ParseArtWorkXML.artWorkRequestID(seleted.artID);
+//				ArtWorkObjectSetUp art = new ArtWorkObjectSetUp(a);
+//				Intent intent = new Intent(c, ArtWorkDetailsActivity.class);
+//				((Activity) c).startActivity(intent);
+			}
+		});
 	}
 }
---
+++ version1.0/src/edu/artAtGVSU/BrowseImageAdapter.java
@@ -0,0 +1,78 @@
+
+package edu.artAtGVSU;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class BrowseImageAdapter extends ArrayAdapter<Bitmap>{
+	int resource;
+    String response;
+    Context context;
+    //Initialize adapter
+    public BrowseImageAdapter(Context context, int resource, List<Bitmap> items) {
+        super(context, resource, items);
+        this.resource=resource;
+ 
+    }
+
+
+	@Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        LinearLayout alertView;
+        //Get the current alert object
+       Bitmap image = getItem(position);
+ 
+        //Inflate the view
+        if(convertView==null)
+        {
+            alertView = new LinearLayout(getContext());
+            String inflater = Context.LAYOUT_INFLATER_SERVICE;
+            LayoutInflater vi;
+            vi = (LayoutInflater)getContext().getSystemService(inflater);
+            vi.inflate(resource, alertView, true);
+        }
+        else
+        {
+            alertView = (LinearLayout) convertView;
+        }
+        //Get the text boxes from the search_list.xml file
+        ImageView artIcon = (ImageView)alertView.findViewById(R.id.buildingIcon);
+        TextView nameText = (TextView)alertView.findViewById(R.id.buildingName);
+ 
+        //Assign the appropriate data from our alert object above
+        if(position == 0){
+        	nameText.setText("Allendale Campus");
+        }else if(position == 1)
+        	nameText.setText("Meijer Campus in Holland");
+        else if(position == 2)
+        	nameText.setText("Muskegon Campus");
+        else if(position == 3)
+        	nameText.setText("PEW Campus (Grand Rapids)");
+        else if(position == 4)
+        	nameText.setText("Traverse City Campus");
+        else
+        	nameText.setText("We lost the records on this site.");
+        
+        artIcon.setImageBitmap(image);
+
+        return alertView;
+    }
+
+}
